ebeec31b66044143f28cfe60664c433e75e68b67
charlotteievans <cie1@rice.edu>
2016-09-28 18:10:41 -0500

    changed names of functions

--- general/curvefitfunctions.py
+++ general/curvefitfunctions.py
@@ -8,10 +8,9 @@ Functions for curve fits
 """
 import numpy as np
 
-def linear(x, M, B):
+def fit_linear(x, M, B):
     return M * x + B
     # makes linear function
 
-
-def cossquared(x, A, B, C):
+def fit_cossquared(x, A, B, C):
     return A * (np.cos(x + B)) ** 2 + C
